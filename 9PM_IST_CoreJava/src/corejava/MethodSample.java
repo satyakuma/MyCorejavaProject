f7504cf04d19a1cd76bdd3c33a3b974e88704ef4
Satya Kumari <Satya Kumari@DESKTOP-5086B0T>
2022-04-01 18:29:26 +0530

    initial commit of my corejava code

---
+++ 9PM_IST_CoreJava/src/corejava/MethodSample.java
@@ -0,0 +1,10 @@
+package corejava;
+
+public class MethodSample {
+
+	public static void main(String[] args) {
+		System.out.println("main method");
+
+	}
+
+}
---
+++ 9PM_IST_CoreJava/src/testing/Test123.java
@@ -0,0 +1,10 @@
+package testing;
+
+public class Test123 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
